7d81ea009abb52b969e247835054f32d7427cf53
Geziel Balan <gzl1910@gmail.com>
2023-10-10 17:49:56 -0600

    Componente Header
    Se creo el componente de Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import Header from './components/Header'
+
 function App() {
 
   return (
     <>
-      <h1>Control de gastos</h1>
+      <Header />
+      <div className='container py-6 md:py-8'>
+        <div className='__row'>
+          <div className='__col'>
+            <h1 className='font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ad quod illum unde at dolorem id quisquam laudantium tempore accusantium quia sit distinctio, possimus minima?</h1>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header className='py-5 lg:py-8 bg-purple-700 sticky top-0 border-b-2 border-purple-500 z-10'>
+        <div className='container'>
+            <div className='__row'>
+                <div className='__col w-full'>
+                    <h3 className=' text-3xl md:text-4xl lg:text-5xl font-semibold text-center'>Control de Gastos</h3>
+                </div>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
